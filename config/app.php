b81e096ff55244c76c4bbead238622daf49fdd93
root <root@Oficina.FERRE.CAT>
2026-09-15 15:29:11 +0200

    Fix syntax error caused by conflict markers in config/app.php

--- config/app.php
+++ config/app.php
@@ -46,11 +46,7 @@ return [
     |
     */
 
-<<<<<<< HEAD
-    'url' => env('APP_URL', 'http://localhost'),
-=======
     'url' => env('APP_URL', 'http://localhost:8085'),
->>>>>>> 190bfe4f015fba0e5e4bad42e53137f7de7ac2d8
 
     /*
     |--------------------------------------------------------------------------
